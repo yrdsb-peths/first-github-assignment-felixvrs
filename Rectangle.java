520caf027f4968a17d023d63a1ecb11a0e1e1f1c
felix <59274976+felixvrs@users.noreply.github.com>
2022-11-11 13:16:15 -0500

    Rectangle class

---
+++ Rectangle.java
@@ -0,0 +1,17 @@
+public class Rectangle{
+
+  private int width;
+  private int height;
+  
+  public Rectangle(rWidth, rHeight)
+  {
+    width = rWidth;
+    height = rHeight;
+  }
+  
+  public String toString()
+  {
+    return "Rectangle with width " + width + " and height " + height;
+  }
+  
+}
